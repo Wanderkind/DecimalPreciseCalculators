2fc7ac62fa5fed9e135a4b0777370cd87663ba4d
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-02-28 16:24:35 +0900

    Update add.py

--- add.py
+++ add.py
@@ -16,6 +16,8 @@ def add(a,b):
     
     l=list(str(int(dc(a,n).replace('.',''))+int(dc(b,n).replace('.',''))).zfill(n+1))
     l.insert(-n,'.')
+    if l[0]+l[1]=='-.':
+        l.insert(1,'0')
     
     return ''.join(l)
 
