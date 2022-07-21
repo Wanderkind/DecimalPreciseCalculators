078b661ed970f825c25962b9407f43cc2ff12768
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-07-21 15:31:50 +0900

    Update Sqrt.py

--- Sqrt.py
+++ Sqrt.py
@@ -1,5 +1,18 @@
 n=400 ###### input number of digits here ######
 
+def dc(a,n):
+    a=str(a)
+    n=int(n)
+    if '.' in a:
+        p=n-len(a)+a.index('.')+1
+        if p>0:
+            a+='0'*p
+        if p<0:
+            a=a[:p]
+    else:
+        a+='.'+'0'*n
+    return a
+
 from math import isqrt
 
 def Sqrt(a):
